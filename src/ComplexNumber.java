680a467921d6d0cd177edbc3ccc78871e47111d4
albert <albert.duenas22@gmail.com>
2022-10-11 21:51:53 -0700

    Final Project

--- src/ComplexNumber.java
+++ src/ComplexNumber.java
@@ -1,2 +1,55 @@
-package PACKAGE_NAME;public class ComplexNumber {
+/*
+* ComplexNumber
+*  Programmer: Albert Duenas
+   Date: 10-11-22
+   Version: 1.2
+   Description: A java class adds and subtracts imaginary numbers. Has private data members imaginaryPart and realPart/
+   * Has getters and setters for private numbers./
+ */
+public class ComplexNumber {
+    //private data members
+    private int imaginaryPart;
+    private int realPart;
+
+    //constructor
+    public ComplexNumber(int imaginaryPart, int realPart){
+        this.imaginaryPart = imaginaryPart;
+        this.realPart = realPart;
+    }
+
+    //getters and setters
+    public void setImaginaryPart(int imaginaryPart) {
+        this.imaginaryPart = imaginaryPart;
+    }
+
+    public int getImaginaryPart() {
+        return imaginaryPart;
+    }
+
+    public void setRealPart(int realPart){
+        this.realPart = realPart;
+    }
+
+    public int getRealPart() {
+        return realPart;
+    }
+    //Method to add two complex numbers
+    public ComplexNumber addComplexNumbers(ComplexNumber a, ComplexNumber b){
+        int sumOfRealParts = a.getRealPart() + b.getRealPart();
+        int sumOfImaginaryParts = a.getImaginaryPart() + b.getImaginaryPart();
+        ComplexNumber sum = new ComplexNumber(sumOfImaginaryParts, sumOfRealParts);
+        return sum;
+    }
+    //Method to subtract two complex numbers
+    public ComplexNumber subtractComplexNumbers(ComplexNumber a, ComplexNumber b){
+        int differenceOfRealParts = a.getRealPart() - b.getRealPart();
+        int differenceOfImaginaryParts = a.getImaginaryPart() - b.getImaginaryPart();
+        ComplexNumber difference = new ComplexNumber(differenceOfImaginaryParts, differenceOfRealParts);
+        return difference;
+    }
+    //Method to print a complex number
+    public void printComplexNumber(ComplexNumber a){
+        System.out.format("%d%+di\r\n", realPart, imaginaryPart);
+
+    }
 }
--- src/ComplexNumberCalculator.java
+++ src/ComplexNumberCalculator.java
@@ -1,2 +1,48 @@
-package PACKAGE_NAME;public class ComplexNumberCalculator {
+/*
+* ComplexNumber
+*  Programmer: Albert Duenas
+   Date: 10-11-22
+   Version: 1.2
+   Description: A java program that adds and subtracts two complex numbers. This program prompts the user for values and
+   *  passes them in to the ComplexNumber constructor.*/
+import java.util.Scanner;
+
+public class ComplexNumberCalculator {
+    public static void main(String[] args){
+        //Declaring new scanner class
+        Scanner sc = new Scanner (System.in);
+
+        //Asking user what the first real part is
+        System.out.println("What is the real part of the first complex number?");
+        String firstRealPartString = sc.nextLine();
+
+        //Asking user what the first imaginary part is
+        System.out.println("What is the imaginary part of the first complex number?");
+        String firstImaginaryPartString = sc.nextLine();
+
+        //Asking user what the second real part is
+        System.out.println("What is the real part of the second complex number?");
+        String secondRealPartString = sc.nextLine();
+
+        //Asking user what the second imaginary part is
+        System.out.println("What is the imaginary part of the second complex number?");
+        String secondImaginaryPartString = sc.nextLine();
+
+        //parsing user inputs
+        int firstRealPart = Integer.parseInt(firstRealPartString);
+        int firstImaginaryPart  = Integer.parseInt(firstImaginaryPartString);
+        int secondRealPart  = Integer.parseInt(secondRealPartString);
+        int secondImaginaryPart  = Integer.parseInt(secondImaginaryPartString);
+        //Creating new complex numbers with user input
+        ComplexNumber a = new ComplexNumber(firstImaginaryPart, firstRealPart);
+        ComplexNumber b = new ComplexNumber(secondImaginaryPart, secondRealPart);
+        //Adding and subtracting user created complex numbers
+        ComplexNumber sum = a.addComplexNumbers(a, b);
+        ComplexNumber difference = a.subtractComplexNumbers(a, b);
+        //Displaying results of addition and subtraction
+        System.out.print("The sum of the two complex numbers is: ");
+        sum.printComplexNumber(sum);
+        System.out.print("The difference of the two complex numbers is: ");
+        difference.printComplexNumber(difference);
+    }
 }
